5b6592796af5966344a40d67925add969caaecf5
Richard Zak <richard.j.zak@gmail.com>
2026-08-13 18:44:06 -0400

    feat(client): release build don't open the terminal on Windows
    Signed-off-by: Richard Zak <richard.j.zak@gmail.com>

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![deny(clippy::all)]
 //#![deny(clippy::cargo)]
 #![deny(clippy::pedantic)]
